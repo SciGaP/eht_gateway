51a50afadf947c03005691f59af4bb06906782bf
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2024-02-26 20:22:54 -0500

    format code

--- htcondor_utilities.py
+++ htcondor_utilities.py
@@ -1,10 +1,10 @@
 """
     htcondor_utilities.py
-        -- assitant function to connect to osg node
+        -- assistant function to connect to osg node
         -- job submission
         -- job status report
     
-    configuation:
+    configuration:
         .env contains ssh connection parameters
             put it the values for the following keys:
                 - userA,sshA,keyA,passA
@@ -13,6 +13,7 @@
 
 import os
 import sys
+
 from dotenv import load_dotenv
 from fabric import Connection
 import re
@@ -20,30 +21,39 @@ import re
 # load server setup
 load_dotenv()
 
+
 def run_ssh_cmd(cmd_str):
     """run a command in ssh"""
 
     gateway = {
-        'host': os.getenv("sshA"),
-        'user': os.getenv("userA"),
-        'connect_kwargs' : {'key_filename':os.path.expanduser(os.getenv("keyA")),'passphrase':os.getenv("passA")}
+        "host": os.getenv("sshA"),
+        "user": os.getenv("userA"),
+        "connect_kwargs": {
+            "key_filename": os.path.expanduser(os.getenv("keyA")),
+            "passphrase": os.getenv("passA"),
+        },
     }
-    
+
     target_host = {
-        'host': os.getenv("sshB"),
-        'user': os.getenv("userB"),
-        'connect_kwargs' : {'key_filename':os.path.expanduser(os.getenv("keyB")),'passphrase':os.getenv("passB")}
+        "host": os.getenv("sshB"),
+        "user": os.getenv("userB"),
+        "connect_kwargs": {
+            "key_filename": os.path.expanduser(os.getenv("keyB")),
+            "passphrase": os.getenv("passB"),
+        },
     }
-    
+
     # Connect to the target host via the gateway
-    with Connection(**target_host,  gateway=Connection(**gateway)) as conn:
+    with Connection(**target_host, gateway=Connection(**gateway)) as conn:
         result = conn.run(cmd_str, hide=True)
         return result
 
+
 def extract_numbers_from_line(line):
     """find all the numbers from line"""
-    numbers = re.findall(r'\d+', line)
+    numbers = re.findall(r"\d+", line)
     return list(map(int, numbers))
+
 
 def htcondor_status():
     """query the htcondor status"""
@@ -56,18 +66,20 @@ def htcondor_status():
     # "Total for query: 50 jobs; 40 completed, 2 removed, 3 idle, 2 running, 2 held, 1 suspended"
     status_line = status_line[0]
     numbers = extract_numbers_from_line(status_line)
-    keys = ["jobs","completed","removed","idle","running","held","suspended"]
+    keys = ["jobs", "completed", "removed", "idle", "running", "held", "suspended"]
     status = dict(zip(keys, numbers))
     return status
 
+
 def main():
-    """ test the routines """
-    output = run_ssh_cmd('hostname')
+    """test the routines"""
+    output = run_ssh_cmd("hostname")
     print("stdout:", output.stdout)
-    print("stderr:",output.stderr)
+    print("stderr:", output.stderr)
 
     status = htcondor_status()
     print(status)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
