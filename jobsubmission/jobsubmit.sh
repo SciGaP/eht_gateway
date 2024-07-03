1b796a4b210de1d936f7652b67d85f474cc16464
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2024-07-03 09:24:32 -0400

    job submit ssh

---
+++ jobsubmission/jobsubmit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if the user provided an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <directory_name>"
+  exit 1
+fi
+
+# Create the directory
+mkdir -p jobs/"$1"
+
+# cp job template folder
+cp -pr job_template jobs/"$1"/job
+
+cd jobs/"$1"/job
+
+# modify submit script
+# in bin/submit, replace batch_name = as batch_name = $1
+
+original_var="haha"
+new_var="\"$1\""
+#echo "$new_var"
+
+# use double quote for variable
+sed -i "s/batch_name =/batch_name = $new_var/g" bin/submit
+
+# submit job
+bin/batches
